0253b5273dc0f892e18d06074b6c68cf02a68b5b
Castellani-Josue <josue.castellani@student.hepl.be>
2023-12-21 23:42:29 +0100

    v2

--- main.py
+++ main.py
@@ -9,12 +9,12 @@ def extract_pages(input_path, output_path, pages_to_extract):
 
         # Ajoute les pages spécifiées
         for page_number in pages_to_extract:
-            # Les pages sont 0-indexées
-            pdf_writer.add_page(pdf_reader.pages[page_number - 1])
+            # Les pages sont 0-indexées => 1ere page à indice 0 et 2eme à 1 et ainsi de suite
+            pdf_writer.add_page(pdf_reader.pages[page_number - 1]) # mais ds notre cas c 1-indexéees donc on f -1
 
         # Écrit le nouveau PDF
         with open(output_path, 'wb') as output_file:
             pdf_writer.write(output_file)
 
-# Exemple d'utilisation : extrait les pages 2 et 17 du fichier dossierStageTFE2023-24.pdf et crée un nouveau fichier document_chrh.pdf
-extract_pages(r'C:\Users\josue\Documents\ECOLE\Stage\dossierStageTFE2023-24.pdf', 'document_chrh.pdf', [2, 17])
+# Exemple d'utilisation : extrait les pages 1 et 2du fichier Énoncé.pdf et crée un nouveau fichier document_exmpl.pdf
+extract_pages(r'C:\Users\josue\Desktop\Dev_Mobile2023_2024\Énoncé.pdf', 'document_exmpl.pdf', [1,2])
